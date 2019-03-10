fed7ac3fc706e1b738faaceccb0342f430741fdb
DenisAydarov <denis.aydarov82@gmail.com>
2019-03-10 23:46:13 +0400

    100319

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
     def create
     	@user = User.new(user_params)
     	if @user.save
-    		#обработать успешное сохранение
+    		flash[:success] = "Welcome to the Sample App!"
+    		redirect_to @user
     	else
     		render 'new'
     	end
@@ -19,7 +20,7 @@ class UsersController < ApplicationController
 
     private
 
-    def user_params
-    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+      def user_params
+    	  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 end
