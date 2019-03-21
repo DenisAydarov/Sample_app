062733f778995b75613daaa84bfd5f79c111147c
DenisAydarov <denis.aydarov82@gmail.com>
2019-03-21 23:24:06 +0400

    210319

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,8 @@ class User < ApplicationRecord
     	update_attribute(:remember_digest, User.digest(remember_token))
     end
     
+    def authenticated?
+    	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+    
 end
