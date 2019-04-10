9cb106e65e542bf71e2c254aaca3949c087d480f
DenisAydarov <denis.aydarov82@gmail.com>
2019-04-10 23:42:39 +0400

    100419

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+    before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user, only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -20,18 +23,12 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
-        if @user.update_attributes(user_params1)
-            #processing successful update
-        else
-            render 'edit'
-        end
     end
 
-    def update                                    #разобраться с этой хуйней, в тестах users_edit_test запрашивает метод update а патч отсылает на edit
-        @user = User.find(params[:id])
+    def update                                    
         if @user.update_attributes(user_params)
-            #processing successful update
+            flash[:success] = "Profile updated!"
+            redirect_to @user
         else
             render 'edit'
         end
@@ -43,7 +40,17 @@ class UsersController < ApplicationController
     	  params.require(:user).permit(:name, :email, :password, :password_confirmation)
       end
 
-      def user_params1                                 #разобраться с этой хуйней, в тестах users_edit_test не работает обычные метод, работает только без require(:user) 
-          params.permit(:name, :email, :password, :password_confirmation)
+      # Предфильтры
+      def logged_in_user  # Подтверждает вход пользователя
+        unless logged_in?
+            flash[:danger] = "Plaese log in"
+            redirect_to login_url
+        end
       end
+
+      def correct_user    # Подтверждает правильного пользователя
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless @user == current_user
+      end
+
 end
