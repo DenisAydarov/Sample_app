1874d02fdd9f61442a7ac5d74ccf79eafc0791d1
DenisAydarov <denis.aydarov82@gmail.com>
2019-04-11 22:44:32 +0400

    11/04/19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
-    before_action :logged_in_user, only: [:edit, :update]
+    before_action :logged_in_user, only: [:index, :edit, :update]
     before_action :correct_user, only: [:edit, :update]
+
+    def index
+    end
 
 	def show
 		@user = User.find(params[:id])
@@ -43,6 +46,7 @@ class UsersController < ApplicationController
       # Предфильтры
       def logged_in_user  # Подтверждает вход пользователя
         unless logged_in?
+            store_location
             flash[:danger] = "Plaese log in"
             redirect_to login_url
         end
@@ -50,7 +54,7 @@ class UsersController < ApplicationController
 
       def correct_user    # Подтверждает правильного пользователя
         @user = User.find(params[:id])
-        redirect_to(root_url) unless @user == current_user
+        redirect_to(root_url) unless current_user?(@user)
       end
 
 end
