c523b532d75964f36ae517d40d9e6ab4202a4982
DenisAydarov <denis.aydarov82@gmail.com>
2019-04-22 22:21:51 +0400

    220419

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -59,4 +59,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {user: { password: "password1", password_confirmation: "password1", admin: true }}
+    assert_not @other_user.admin?
+  end
+
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,8 +13,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
     get signup_path
     assert_select "title", full_title("Sign up")
+    
   end
   
 end
